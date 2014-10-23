c9c4ccfa980ea4a7c25088ad82b839a7fb341190
Jan Horava <czechinthemail@gmail.com>
2014-10-23 14:30:12 -0700

    improved formatting

--- js/slots.js
+++ js/slots.js
@@ -3,7 +3,7 @@ function play_round(){
     document.getElementById('number-second').innerHTML = Math.floor(Math.random() * 10);
     document.getElementById('number-third').innerHTML = Math.floor(Math.random() * 10);
 
-    // if all numbers match
+    // If all numbers match...
     if(document.getElementById('number-first').innerHTML == document.getElementById('number-second').innerHTML
       && document.getElementById('number-second').innerHTML == document.getElementById('number-third').innerHTML){
         document.getElementById('result').innerHTML = 
@@ -11,10 +11,10 @@ function play_round(){
           + (parseInt(document.getElementById('number-first').innerHTML) * 2)
           + ' coins!';
 
-        // give player twice as many coins as the numbers that matched
+        // ...give player twice as many coins as the numbers that matched.
         coins += parseInt(document.getElementById('number-first').innerHTML) * 2;
 
-    // if first number matches either of the other two numbers
+    // If first number matches either of the other two numbers...
     }else if(document.getElementById('number-first').innerHTML == document.getElementById('number-second').innerHTML
       || document.getElementById('number-first').innerHTML == document.getElementById('number-third').innerHTML){
         document.getElementById('result').innerHTML = 
@@ -22,28 +22,28 @@ function play_round(){
           + document.getElementById('number-first').innerHTML
           + ' coins.';
 
-        // give player as many coins as the first number
+        // ...give player as many coins as the first number.
         coins += parseInt(document.getElementById('number-first').innerHTML);
 
-    // if the second and third numbers match
+    // If the second and third numbers match...
     }else if(document.getElementById('number-second').innerHTML == document.getElementById('number-third').innerHTML){
         document.getElementById('result').innerHTML = 
           'Two Match. +'
           + document.getElementById('number-second').innerHTML
           + ' coins.';
 
-        // give player as many coins as second number
+        // ...give player as many coins as second number.
         coins += parseInt(document.getElementById('number-second').innerHTML);
 
-    // if no numbers match
+    // If no numbers match...
     }else{
         document.getElementById('result').innerHTML = 'No Match... -2 coins...';
 
-        // take away two coins from player
+        // ...take away two coins from player.
         coins -= 2;
     }
 
-    // update coins
+    // Update remaining coins.
     document.getElementById('coins').innerHTML = coins;
 }
 
@@ -66,7 +66,8 @@ window.onkeydown = function(e){
     var key = window.event ? event : e;
     key = key.charCode ? key.charCode : key.keyCode;
 
-    if(key == 83){ // S
+    // S: play a round.
+    if(key == 83){
         play_round();
     }
 };
