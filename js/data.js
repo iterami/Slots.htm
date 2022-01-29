9b44b40ae467d6e2c903c95e35529920cedf2d18
Jan Hořava <czechinthemail@gmail.com>
2022-01-29 13:40:29 -0800

    added number type to settings inputs, removed unnecessary comments

--- js/data.js
+++ js/data.js
@@ -13,30 +13,21 @@ function play_round(){
       'max': 10,
     });
 
-    // If all numbers match...
     if(first === second
       && second === third){
-        // ...give player twice as many coins as the numbers that matched.
         dcoins = (first * 3) * core_storage_data['multiplier'];
         result = 'Three Match! +' + dcoins + ' coins!';
 
-    // If first number matches either of the other two numbers...
     }else if(first === second
       || first === third){
-        // ...give player as many coins as the first number.
         dcoins = first * core_storage_data['multiplier'];
         result = 'Two Match. +' + dcoins + ' coins.';
 
-
-    // If the second and third numbers match...
     }else if(second === third){
-        // ...give player as many coins as second number.
         dcoins = second * core_storage_data['multiplier'];
         result = 'Two Match. +' + dcoins + ' coins.';
 
-    // If no numbers match...
     }else{
-        // ...take away two coins from player.
         dcoins = -2 * core_storage_data['multiplier'];
         result = 'No Match... ' + dcoins + ' coins...';
     }
