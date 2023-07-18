cb208fba2129a4554ddc370dcdffa747cab5035e
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 14:47:09 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -42,3 +42,25 @@ function play_round(){
 
     core_storage_update();
 }
+
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'play': {
+          'onclick': play_round,
+        },
+      },
+      'keybinds': {
+        13: {
+          'todo': play_round,
+        },
+      },
+      'storage': {
+        'coins': 100,
+        'multiplier': 1,
+        'total': 0,
+      },
+      'storage-menu': '<table><tr><td><input class=mini id=multiplier step=any type=number><td>Multiplier</table>',
+      'title': 'Slots.htm',
+    });
+}
